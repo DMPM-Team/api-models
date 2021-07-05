6ef1efac160a9858e1a81b647a5c37f3b95017e1
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2021-07-05 16:54:43 +0100

    Initial Commit - Base API Models

---
+++ PackageBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DMPackageManager.Api.Models {
+    /// <summary>
+    /// Base model for all package related API responses
+    /// </summary>
+    class PackageBase {
+        /// <summary>
+        /// Name of the package
+        /// </summary>
+        public string package_name { get; set; }
+
+        /// <summary>
+        /// Package version
+        /// </summary>
+        public Version package_version { get; set; }
+
+        /// <summary>
+        /// UTC timestamp of when this package was first released
+        /// </summary>
+        public DateTime release_date { get; set; }
+
+        /// <summary>
+        /// UTC timestamp of when this package was last updated
+        /// </summary>
+        public DateTime update_date { get; set; }
+    }
+}
---
+++ PackageByVersion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DMPackageManager.Api.Models {
+    class PackageByVersion : PackageBase {
+        /// <summary>
+        /// Changelog for this version of the package
+        /// </summary>
+        public string changelog { get; set; }
+
+        /// <summary>
+        /// Latest version of this package. You can compare this to the <see cref="package_version"/> field to check if the package is the latest version.
+        /// </summary>
+        public Version latest_version { get; set; }
+
+        /// <summary>
+        /// Is this version deemed "vulnerable" and should the user update ASAP?
+        /// </summary>
+        public bool version_vulnerable { get; set; }
+    }
+}
