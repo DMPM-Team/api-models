b6533c70eb261e09ac69668ecf1973e7519db595
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2021-07-09 18:59:04 +0100

    New models

---
+++ CliVersion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DMPackageManager.Api.Models {
+    /// <summary>
+    /// Simple class to get the latest version of the CLI. Used by the CLI client to see if an update is available
+    /// </summary>
+    public class CliVersion {
+        /// <summary>
+        /// Get the latest version of the CLI
+        /// </summary>
+        public string latest_version { get; set; }
+    }
+}
--- PackageSpec.cs
+++ PackageSpec.cs
@@ -4,18 +4,23 @@ using System.Text;
 
 namespace DMPackageManager.Api.Models {
     /// <summary>
-    /// Base model for all package related API responses
+    /// Package specification to be returned by the API
     /// </summary>
-    class PackageBase {
+    public class PackageSpec {
         /// <summary>
         /// Name of the package
         /// </summary>
         public string package_name { get; set; }
 
         /// <summary>
-        /// Package version
+        /// Description of the package
         /// </summary>
-        public Version package_version { get; set; }
+        public string package_desc { get; set; }
+
+        /// <summary>
+        /// Latest version of the package
+        /// </summary>
+        public string latest_version { get; set; }
 
         /// <summary>
         /// UTC timestamp of when this package was first released
@@ -26,5 +31,10 @@ namespace DMPackageManager.Api.Models {
         /// UTC timestamp of when this package was last updated
         /// </summary>
         public DateTime update_date { get; set; }
+
+        /// <summary>
+        /// List of all versions this package has available
+        /// </summary>
+        public List<string> all_versions { get; set; }
     }
 }
